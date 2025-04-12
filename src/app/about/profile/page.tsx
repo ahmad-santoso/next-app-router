5898209740d03a5adb8f1e93c1f2fe9bad7cca78
Ahmad Santoso <ahmadsantoso800@gmail.com>
2025-04-12 22:40:32 +0700

    feat: implement layout and template routing

--- src/app/about/profile/page.tsx
+++ src/app/about/profile/page.tsx
@@ -1,9 +1,9 @@
-import Navbar from "./navbar";
+import Navbar from "../../navbar";
 
 export default function ProfilePage() {
     return (
       <div>
-        <Navbar></Navbar>
+        <h1>Profile Page</h1>
       </div>
     )
   }
