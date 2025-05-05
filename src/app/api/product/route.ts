7ea9d518695e89960d760f0421fa9fa388b75ff6
Ahmad Santoso <ahmadsantoso800@gmail.com>
2025-05-05 22:55:49 +0700

    feat(product): fetch product data from local API and Fake Store API

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,15 +1,18 @@
 import { NextResponse, NextRequest } from "next/server";
+import { title } from "process";
 
 const data = [
     {
         id: 1,
-        name: 'Sepatu Baru',
-        price: 100000
+        title: 'Sepatu Baru',
+        price: 100000,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/AIR+FORCE+1+%2707.png"
     },
     {
         id: 2,
-        name: 'Sepatu Baru Dong',
-        price: 10000000
+        title: 'Sepatu Baru Dong',
+        price: 10000000,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/AIR+FORCE+1+%2707.png"
     }
 ]
 
